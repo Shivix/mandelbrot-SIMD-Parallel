959c612fed4b443402cba4465ad3af1ffe4f2938
Mark Oborne <markoborne1@gmail.com>
2021-01-15 07:21:15 +0000

    improved simd of point computation

--- main.cpp
+++ main.cpp
@@ -27,37 +27,6 @@ Vec4db escape_check(const ComplexVec4d z){
     return (z.real * z.real + z.imag * z.imag) > 4;
 }
 
-Vec4i compute_point(const ComplexVec4d c){ // TODO: improve
-    constexpr int MAX_ITER{255 * 4};
-    ComplexVec4d z{0, 0};
-    Vec4i result{MAX_ITER, MAX_ITER, MAX_ITER, MAX_ITER};
-    Vec4ib r{false, false, false, false};
-    for(int i{1}; i <= MAX_ITER; ++i){
-        z = mandelbrot(z, c);
-        auto iters{escape_check(z)};
-        if (iters[0] && !r[0]){
-            result.insert(0, i);
-            r.insert(0, true);
-        }
-        if (iters[1] && !r[1]){
-            result.insert(1, i);
-            r.insert(1, true);
-        }
-        if (iters[2] && !r[2]){
-            result.insert(2, i);
-            r.insert(2, true);
-        }
-        if (iters[3] && !r[3]){
-            result.insert(3, i);
-            r.insert(3, true);
-        }
-        if(horizontal_and(r)){
-            return result;
-        }
-    }
-    return result;
-}
-
 Vec4d pixel_to_mandelbrot(const Vec4d pixel, const double offset, const double zoom, const int zoom_correction){
     return (pixel - zoom_correction) * zoom + offset;
 }
@@ -65,22 +34,32 @@ Vec4d pixel_to_mandelbrot(const Vec4d pixel, const double offset, const double z
 // Utilizes AVX2 for SIMD  --  256 bit registers with 4 doubles
 void render_mandelbrot(cv::Mat& mandelbrot_set, const double x_offset, const double y_offset, const double zoom,
                        const int width, const int height, const int width_start = 0, const int height_start = 0){
+    constexpr int MAX_ITER{255 * 4};
+    const Vec4d ONE{1, 1, 1, 1};
     auto width_correction{mandelbrot_set.cols / 2};
     auto height_correction{mandelbrot_set.rows / 2};
     for(auto i{height_start}; i < height; ++i){
         for(auto j{width_start}; j < width; j+=4){
             Vec4d pixels{static_cast<double>(j), static_cast<double>(j+1), static_cast<double>(j+2), static_cast<double>(j+3)};
-            const Vec4i num_of_iters{compute_point({pixel_to_mandelbrot(pixels, x_offset, zoom, width_correction),
-                                                   pixel_to_mandelbrot(i, y_offset, zoom, height_correction)})};
+            ComplexVec4d c{pixel_to_mandelbrot(pixels, x_offset, zoom, width_correction),
+                           pixel_to_mandelbrot(i, y_offset, zoom, height_correction)};
+
+            Vec4d num_of_iters{0, 0, 0, 0};
+            ComplexVec4d z{0, 0};
+            int test{0};
+            while(++test <= MAX_ITER){ // TODO: I think some values are still adding after escaping
+                z = mandelbrot(z, c);
+                const Vec4db escape_mask{escape_check(z)};
+                if (_mm256_movemask_pd(escape_mask) >= 4){
+                    break;
+                }
+                num_of_iters += ONE & (~escape_mask);
+            }
             
-            const auto colour{static_cast<unsigned char>(num_of_iters[0] % 255)};
-            mandelbrot_set.at<cv::Vec3b>(i, j) = cv::Vec3b{colour, colour, colour};
-            const auto colour2{static_cast<unsigned char>(num_of_iters[1] % 255)};
-            mandelbrot_set.at<cv::Vec3b>(i, j + 1) = cv::Vec3b{colour2, colour2, colour2};
-            const auto colour3{static_cast<unsigned char>(num_of_iters[2] % 255)};
-            mandelbrot_set.at<cv::Vec3b>(i, j + 2) = cv::Vec3b{colour3, colour3, colour3};
-            const auto colour4{static_cast<unsigned char>(num_of_iters[3] % 255)};
-            mandelbrot_set.at<cv::Vec3b>(i, j + 3) = cv::Vec3b{colour4, colour4, colour4};
+            for (int index{0}; index < 4; ++index){
+                const auto colour{static_cast<unsigned char>(static_cast<int>(num_of_iters[index]) % 255)};
+                mandelbrot_set.at<cv::Vec3b>(i, j + index) = cv::Vec3b{colour, colour, colour};
+            }
         }
     }
 }
@@ -153,10 +132,10 @@ void render_mandelbrot_threaded(std::array<std::thread, 4>& thread_pool, cv::Mat
 int main(){
     constexpr int WIDTH{1920};
     constexpr int HEIGHT{1080};
-    
+
     cv::Mat mandelbrot_set{cv::Size(WIDTH, HEIGHT), CV_8UC3, cv::Scalar(30, 30, 30)};
     std::array<std::thread, 4> thread_pool;
-    
+
     double zoom{0.003};
     double x_offset{0.0};
     double y_offset{0.0};
